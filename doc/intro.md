cf97e4ba1f097fb965a89108b0b929fecdfb2f7d
Zachary Smith <zsmith0@gmail.com>
2017-08-29 08:49:42 -0400

    first commit: simple lexer implemented

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to climp
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/climp/core_test.clj
@@ -0,0 +1,8 @@
+(ns climp.core-test
+  (:require [clojure.test :refer :all]
+            [midje.sweet :refer :all]
+            [climp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
